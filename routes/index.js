5eb46224bd4744538c42f4852f7ed77caa8100d3
Wes Hediger <wes.hediger@gmail.com>
2016-06-05 21:14:45 -0600

    add routes/index.js inital commit

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,12 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('http://api.themoviedb.org/3/movie/upcoming/?api_key=b2c319d64cba3280f7ee6977b9a470e0', function(request, response){
+  console.log(response);
+  // response.render('how-to-shoot-a-bow', { steps: information.firstSteps,
+  //   title: 'First Steps',
+  //   back: '/how-to-shoot-a-bow-part-3',
+  //   next: "/how-to-shoot-a-bow-part-2"})
+});
+
 module.exports = router;
