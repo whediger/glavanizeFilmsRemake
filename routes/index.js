94646db5212ca351754042f737f2e81874737bbf
Wes Hediger <wes.hediger@gmail.com>
2016-06-05 22:37:01 -0600

    add promise function for movie data to routes

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,21 @@ var express = require('express');
 var request = require('request');
 var router = express.Router();
 
+function getMovieData() {
+  return new Promise(function(fulfill, reject){
+    request('http://api.themoviedb.org/3/movie/upcoming/?api_key=b2c319d64cba3280f7ee6977b9a470e0',
+      function(error, response, data){
+        if (error) reject(error);
+        else fulfill(data);
+      });
+  });
+}
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Movie Guide' });
+//var movieData =
+getMovieData().then(function(movieData){console.log(movieData)});
 
 
     // response.render('how-to-shoot-a-bow', { steps: information.firstSteps,
