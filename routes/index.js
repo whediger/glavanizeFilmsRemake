0521849643d2eaaba22955f76b7e196e7f796051
Wes Hediger <wes.hediger@gmail.com>
2016-06-05 22:04:58 -0600

    add request Module

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,18 @@
 var express = require('express');
+var request = require('request');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Movie Guide' });
+
+
+    // response.render('how-to-shoot-a-bow', { steps: information.firstSteps,
+    //   title: 'First Steps',
+    //   back: '/how-to-shoot-a-bow-part-3',
+    //   next: "/how-to-shoot-a-bow-part-2"})
+
 });
 
-router.get('http://api.themoviedb.org/3/movie/upcoming/?api_key=b2c319d64cba3280f7ee6977b9a470e0', function(request, response){
-  console.log("response");
-  // response.render('how-to-shoot-a-bow', { steps: information.firstSteps,
-  //   title: 'First Steps',
-  //   back: '/how-to-shoot-a-bow-part-3',
-  //   next: "/how-to-shoot-a-bow-part-2"})
-});
-
+console.log("hello out thereeeeee!!!!!!!        +=={========>")
 module.exports = router;
