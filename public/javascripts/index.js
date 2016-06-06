c4cede0702dd8f85d6643695d8e44eb69a6d6af7
Wes Hediger <wes.hediger@gmail.com>
2016-06-05 20:53:17 -0600

    fix custome google maps pointer icon

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,6 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
-
+router.get('http://api.themoviedb.org/3/movie/upcoming/?api_key=b2c319d64cba3280f7ee6977b9a470e0', function(request, response){
+  console.log(response);
+  // response.render('how-to-shoot-a-bow', { steps: information.firstSteps,
+  //   title: 'First Steps',
+  //   back: '/how-to-shoot-a-bow-part-3',
+  //   next: "/how-to-shoot-a-bow-part-2"})
+});
 
 module.exports = router;
