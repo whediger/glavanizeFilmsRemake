de1187a07c6c0b3ba65d7c96b1a9cd7abed0f1df
Wes Hediger <wes.hediger@gmail.com>
2016-06-08 13:52:51 -0600

    add un-functional star rating

---
+++ public/javascripts/movie.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+  var numberOfMovies = $('.panel-group').data('numOfMovies');
+  console.log(numberOfMovies);
+  // for ( var i = 0; i < $())
+  // $(.voteRating:nth-of-type(i)).
+
+});
--- routes/index.js
+++ routes/index.js
@@ -6,40 +6,60 @@ function getMovieData() {
   return new Promise(function(fulfill, reject){
     request('https://api.themoviedb.org/3/movie/upcoming/?api_key=b2c319d64cba3280f7ee6977b9a470e0',
       function(error, response, data){
+        // console.log('data ', data);
+        // console.log('response ', response);
         if (error) reject(error);
         else fulfill(data);
       });
   });
 }
 
+function getMonth(dateIn) {
+  //var date = movieData.results[i].release_date
+  var months = [ 'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec' ];
+  var dateKey = dateIn.split(""); //split date into array of chars to use
+  //console.log(dateKey); //uncomment to see dateKey array
+  //if month number is less than 10
+  if ( dateKey[5] === '0' ) {
+    date = months[dateKey[6]] + " " + dateKey[8] + dateKey[9];
+  } else {
+    //else convert dateKey val from string to int and add 10
+    date = months[(parseInt(dateKey[6]) + 10)] + " " + dateKey[8] + dateKey[9];
+  }
+  return date;
+}
+
+function getPopularity(voteIn) {
+  return Math.round(voteIn);
+}
+
 /* GET home page. */
 router.get('/', function(request, response, next) {
   //response.render('index', { title: 'Movie Guide' });
-
   getMovieData().then(function(data){
     var movieData = JSON.parse(data);
-    var movies = { movies: [] };
+    var movieNum = 10; //this is the number of movies displayed
+    var movies = { numberOfMovies: movieNum,
+                   movies: [] };
+    var date = "";
+    var vote = 0;
 
-    for ( var i = 0; i < 10; i++ ) {
-      var date = movieData.results[i].release_date
-      var months = [ 'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec' ];
-      var dateKey = date.split(""); //split date into array of chars to use
-      //console.log(dateKey); //uncomment to see dateKey array
-      //if month number is less than 10
-      if ( dateKey[5] === '0' ) {
-      date = months[dateKey[6]] + " " + dateKey[8] + dateKey[9];
-    } else {
-      //else convert dateKey val from string to int and add 10
-      date = months[(parseInt(dateKey[6]) + 10)] + " " + dateKey[8] + dateKey[9];
-    }
-      movies.movies[i] = { title: movieData.results[i].title,
+    for ( var i = 0; i < movieNum; i++ ) {
+      date = getMonth(movieData.results[i].release_date);
+      vote = getPopularity(movieData.results[i].popularity);
+
+      movies.movies[i] = {  title: movieData.results[i].title,
                             movieId: "movie" + i,
                             moviePic: 'http://image.tmdb.org/t/p/w500/'
                                       + movieData.results[i].poster_path,
-                            releaseDate: date};
+                            releaseDate: date,
+                            movieCopy: movieData.results[i].overview,
+                            numberOfMovies: movieNum,
+                            votes: vote
+                            };
 
-      //console.log(movies.movies[i].title);
     }
+    console.log(movies);
     //console.log(movies);
 
     response.render('index', movies);
