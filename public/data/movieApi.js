4a6d957660a73a351fef4509584a2e036ab179aa
Wes Hediger <wes.hediger@gmail.com>
2016-10-23 14:09:13 -0600

    move getDate to movieApi module

--- public/data/movieApi.js
+++ public/data/movieApi.js
@@ -13,17 +13,36 @@ module.exports = {
           else fulfill(data);
         });
     });
+  },
+
+  getMonth: function(dateIn) {
+    //var date = movieData.results[i].release_date
+    var months = [ 'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec' ];
+    var dateKey = dateIn.split(""); //split date into array of chars to use
+    //console.log(dateKey); //uncomment to see dateKey array
+    //if month number is less than 10
+    if ( dateKey[5] === '0' ) {
+      date = months[dateKey[6]] + " " + dateKey[8] + dateKey[9];
+    } else {
+      //else convert dateKey val from string to int and add 10
+      date = months[(parseInt(dateKey[6]) + 10)] + " " + dateKey[8] + dateKey[9];
+    }
+    return date;
   }
 }
 
-// function getUpcomingMovieData() {
-//   return new Promise(function(fulfill, reject){
-//     request('https://api.themoviedb.org/3/movie/upcoming/?sort_by=popularity.desc&api_key=b2c319d64cba3280f7ee6977b9a470e0',
-//       function(error, response, data){
-//         // console.log('data ', data);
-//         // console.log('response ', response);
-//         if (error) reject(error);
-//         else fulfill(data);
-//       });
-//   });
+//
+// function getMonth(dateIn) {
+//   //var date = movieData.results[i].release_date
+//   var months = [ 'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec' ];
+//   var dateKey = dateIn.split(""); //split date into array of chars to use
+//   //console.log(dateKey); //uncomment to see dateKey array
+//   //if month number is less than 10
+//   if ( dateKey[5] === '0' ) {
+//     date = months[dateKey[6]] + " " + dateKey[8] + dateKey[9];
+//   } else {
+//     //else convert dateKey val from string to int and add 10
+//     date = months[(parseInt(dateKey[6]) + 10)] + " " + dateKey[8] + dateKey[9];
+//   }
+//   return date;
 // }
--- routes/index.js
+++ routes/index.js
@@ -5,23 +5,6 @@ var movieApi = require('../public/data/movieApi');
 
 
 
-
-
-function getMonth(dateIn) {
-  //var date = movieData.results[i].release_date
-  var months = [ 'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec' ];
-  var dateKey = dateIn.split(""); //split date into array of chars to use
-  //console.log(dateKey); //uncomment to see dateKey array
-  //if month number is less than 10
-  if ( dateKey[5] === '0' ) {
-    date = months[dateKey[6]] + " " + dateKey[8] + dateKey[9];
-  } else {
-    //else convert dateKey val from string to int and add 10
-    date = months[(parseInt(dateKey[6]) + 10)] + " " + dateKey[8] + dateKey[9];
-  }
-  return date;
-}
-
 function getPopularity(voteIn) {
   return Math.round(voteIn);
 }
@@ -41,7 +24,7 @@ router.get('/', function(request, response, next) {
     for ( var i = 0; i < movieNum; i++ ) {
       var isoDate = new Date(movieData.results[i].release_date);
       isoDate = isoDate.toISOString().replace(/-|:|\.\d\d\d/g,"");
-      date = getMonth(movieData.results[i].release_date);
+      date = movieApi.getMonth(movieData.results[i].release_date);
       popular = getPopularity(movieData.results[i].popularity);
       genreNames = movieData.results[i].genre_ids;
       //genre.getGenre(genreNames);
