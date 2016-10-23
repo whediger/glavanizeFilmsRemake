558c40d0e5fe50054aed739f7495e1ac4cc6afc7
Wes Hediger <wes.hediger@gmail.com>
2016-10-23 14:24:18 -0600

    move genre module to module folder

--- public/data/movieApi.js
+++ public/data/movieApi.js
@@ -1,5 +1,6 @@
 
 var request = require('request');
+var genre = require('./genre');
 
 
 module.exports = {
@@ -32,6 +33,38 @@ module.exports = {
 
   getPopularity: function(voteIn) {
       return Math.round(voteIn);
+  },
+
+  filterUpComingMovieData: function(data){
+    var movieData = JSON.parse(data);
+    var movieNum = 10; //this is the number of movies displayed
+    var upComingMovies = { numberOfMovies: movieNum,
+                           movies: [] };
+    var date = "";
+    var vote = 0;
+    //todo --votes and popularity ar backwards
+    for ( var i = 0; i < movieNum; i++ ) {
+      var isoDate = new Date(movieData.results[i].release_date);
+      isoDate = isoDate.toISOString().replace(/-|:|\.\d\d\d/g,"");
+      date = this.getMonth(movieData.results[i].release_date);
+      popular = this.getPopularity(movieData.results[i].popularity);
+      genreNames = movieData.results[i].genre_ids;
+      //genre.getGenre(genreNames);
+
+      upComingMovies.movies[i] = {  title: movieData.results[i].title,
+                            movieId: "movie" + i,
+                            moviePic: 'http://image.tmdb.org/t/p/w500/'
+                                      + movieData.results[i].poster_path,
+                            releaseDate: date,
+                            isoDate: isoDate,
+                            movieCopy: movieData.results[i].overview,
+                            numberOfMovies: movieNum,
+                            popularity: popular,
+                            votes: movieData.results[i].vote_count,
+                            genre: genre.getGenre(genreNames)
+                            };
+    }
+    return upComingMovies;
   }
 }
 
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,4 @@
 var express = require('express');
-var genre = require('./genre');
 var router = express.Router();
 var movieApi = require('../public/data/movieApi');
 
@@ -8,34 +7,8 @@ var movieApi = require('../public/data/movieApi');
 router.get('/', function(request, response, next) {
   //response.render('index', { title: 'Movie Guide' });
   movieApi.getUpcomingMovieData().then(function(data){
-    var movieData = JSON.parse(data);
-    var movieNum = 10; //this is the number of movies displayed
-    var upComingMovies = { numberOfMovies: movieNum,
-                           movies: [] };
-    var date = "";
-    var vote = 0;
-    //todo --votes and popularity ar backwards
-    for ( var i = 0; i < movieNum; i++ ) {
-      var isoDate = new Date(movieData.results[i].release_date);
-      isoDate = isoDate.toISOString().replace(/-|:|\.\d\d\d/g,"");
-      date = movieApi.getMonth(movieData.results[i].release_date);
-      popular = movieApi.getPopularity(movieData.results[i].popularity);
-      genreNames = movieData.results[i].genre_ids;
-      //genre.getGenre(genreNames);
 
-      upComingMovies.movies[i] = {  title: movieData.results[i].title,
-                            movieId: "movie" + i,
-                            moviePic: 'http://image.tmdb.org/t/p/w500/'
-                                      + movieData.results[i].poster_path,
-                            releaseDate: date,
-                            isoDate: isoDate,
-                            movieCopy: movieData.results[i].overview,
-                            numberOfMovies: movieNum,
-                            popularity: popular,
-                            votes: movieData.results[i].vote_count,
-                            genre: genre.getGenre(genreNames)
-                            };
-    }
+    var upComingMovies = movieApi.filterUpComingMovieData(data);
     //console.log(movies);
 
     response.render('index', upComingMovies);
