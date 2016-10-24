052b51924b032b329af51a3868660ff10766bc13
Wes Hediger <wes.hediger@gmail.com>
2016-10-24 13:48:17 -0600

    fix date bug

--- public/data/movieApi.js
+++ public/data/movieApi.js
@@ -18,16 +18,17 @@ module.exports = {
   },
 
   getMonth: function(dateIn) {
+    console.log(dateIn);
     //var date = movieData.results[i].release_date
     var months = [ 'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec' ];
     var dateKey = dateIn.split(""); //split date into array of chars to use
     //console.log(dateKey); //uncomment to see dateKey array
     //if month number is less than 10
     if ( dateKey[5] === '0' ) {
-      date = months[dateKey[6]] + " " + dateKey[8] + dateKey[9];
+      date = months[dateKey[6] -1] + " " + dateKey[8] + dateKey[9];
     } else {
       //else convert dateKey val from string to int and add 10
-      date = months[(parseInt(dateKey[6]) + 10)] + " " + dateKey[8] + dateKey[9];
+      date = months[(parseInt(dateKey[6]) + 10) -1] + " " + dateKey[8] + dateKey[9];
     }
     return date;
   },
