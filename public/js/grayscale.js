dcf5b9c4986ec916263af156c5e654106767888d
Wes Hediger <wes.hediger@gmail.com>
2016-10-27 00:13:48 -0600

    remove AJAX and create initMap zipcode dependency injection function

--- public/js/grayscale.js
+++ public/js/grayscale.js
@@ -34,33 +34,20 @@ $('.navbar-collapse ul li a').click(function() {
   }
 });
 
-//AJAX functions
-function getAsyncAPI(url, callback){
-  var xmlHttp = new XMLHttpRequest();
-  xmlHttp.onreadystatechange = function() {
-    if (xmlHttp.readyState == 4 && xmlHttp.status == 200){
-      callback(xmlHttp.responseText);
-    }
-    xmlHttp.open("GET", url, true);
-    xmlHttp.send(null);
-  }
-}
 
 //google maps geocoder
-function getUserLocation(zipCode) {
-  console.log('in get user location');
+function getUserLocation(zipCode, callback) {
   return new Promise(function(fulfill, reject){
-    getAsyncAPI('https://maps.googleapis.com/maps/api/geocode/json?address='+ zipCode + '&components=postal_code&key=AIzaSyAroEaSDeI9YCYmgcbMWqLIybCv8XfY6pA',
-      function(error, response, geoLocation){
+    $.get('https://maps.googleapis.com/maps/api/geocode/json?address='+ zipCode + '&components=postal_code&key=AIzaSyAroEaSDeI9YCYmgcbMWqLIybCv8XfY6pA',
+      function(geoLocation){
         var location = {};
-        geoLocation = JSON.parse(geoLocation);
+        //geoLocation = JSON.parse(geoLocation);
         // console.log(geoLocation.results[0].geometry.location.lat);
         // console.log(geoLocation.results[0].geometry.location.lng);
         location = {  lat: geoLocation.results[0].geometry.location.lat,
                       lng: geoLocation.results[0].geometry.location.lng,
                   zipCode: zipCode }
-        if (error) reject(error)
-        else fulfill(location)
+        callback(location);
       });
   });
 }
@@ -68,159 +55,161 @@ function getUserLocation(zipCode) {
 // Google Maps Scripts
 var map = null;
 // When the window has finished loading create our google map below
-google.maps.event.addDomListener(window, 'load', function(){
-    getUserLocation(80209)
-     .then(function(location){
-       init();
-     });
-});
+google.maps.event.addDomListener(window, 'load', initMap(80209) );
 
 
+function initMap(zipCode){
+  // dependency injection - geocode the zipcode
+  getUserLocation(zipCode, init);
+  document.getElementById('zipCodeInput').placeholder = zipCode;
+  google.maps.event.addDomListener(window, 'resize', function() {
+      getUserLocation(zipCode, function(location){
+          map.setCenter(new google.maps.LatLng(location.lat, location.lng)); //Denver 39.7376, -104.9897
+      });
+  });
+}
 
-google.maps.event.addDomListener(window, 'resize', function() {
-    map.setCenter(new google.maps.LatLng(39.7376, -104.9897)); //Denver
-});
+function init(location) {
+      // Basic options for a simple Google Map
+      // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
+      var mapOptions = {
+          // How zoomed in you want the map to start at (always required)
+          zoom: 12,
 
-function init() {
-    // Basic options for a simple Google Map
-    // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
-    var mapOptions = {
-        // How zoomed in you want the map to start at (always required)
-        zoom: 12,
+          // The latitude and longitude to center the map (always required)
+          center: new google.maps.LatLng(location.lat, location.lng), // Denver 39.7376, -104.9897
 
-        // The latitude and longitude to center the map (always required)
-        center: new google.maps.LatLng(39.7376, -104.9897), // Denver
+          // Disables the default Google Maps UI components
+          disableDefaultUI: true,
+          scrollwheel: false,
+          draggable: false,
 
-        // Disables the default Google Maps UI components
-        disableDefaultUI: true,
-        scrollwheel: false,
-        draggable: false,
+          // How you would like to style the map.
+          // This is where you would paste any style found on Snazzy Maps.
+          styles: [{
+              "featureType": "water",
+              "elementType": "geometry",
+              "stylers": [{
+                  "color": "#000000"
+              }, {
+                  "lightness": 17
+              }]
+          }, {
+              "featureType": "landscape",
+              "elementType": "geometry",
+              "stylers": [{
+                  "color": "#000000"
+              }, {
+                  "lightness": 20
+              }]
+          }, {
+              "featureType": "road.highway",
+              "elementType": "geometry.fill",
+              "stylers": [{
+                  "color": "#000000"
+              }, {
+                  "lightness": 17
+              }]
+          }, {
+              "featureType": "road.highway",
+              "elementType": "geometry.stroke",
+              "stylers": [{
+                  "color": "#000000"
+              }, {
+                  "lightness": 29
+              }, {
+                  "weight": 0.2
+              }]
+          }, {
+              "featureType": "road.arterial",
+              "elementType": "geometry",
+              "stylers": [{
+                  "color": "#000000"
+              }, {
+                  "lightness": 18
+              }]
+          }, {
+              "featureType": "road.local",
+              "elementType": "geometry",
+              "stylers": [{
+                  "color": "#000000"
+              }, {
+                  "lightness": 16
+              }]
+          }, {
+              "featureType": "poi",
+              "elementType": "geometry",
+              "stylers": [{
+                  "color": "#000000"
+              }, {
+                  "lightness": 21
+              }]
+          }, {
+              "elementType": "labels.text.stroke",
+              "stylers": [{
+                  "visibility": "on"
+              }, {
+                  "color": "#000000"
+              }, {
+                  "lightness": 16
+              }]
+          }, {
+              "elementType": "labels.text.fill",
+              "stylers": [{
+                  "saturation": 36
+              }, {
+                  "color": "#000000"
+              }, {
+                  "lightness": 40
+              }]
+          }, {
+              "elementType": "labels.icon",
+              "stylers": [{
+                  "visibility": "off"
+              }]
+          }, {
+              "featureType": "transit",
+              "elementType": "geometry",
+              "stylers": [{
+                  "color": "#000000"
+              }, {
+                  "lightness": 19
+              }]
+          }, {
+              "featureType": "administrative",
+              "elementType": "geometry.fill",
+              "stylers": [{
+                  "color": "#000000"
+              }, {
+                  "lightness": 20
+              }]
+          }, {
+              "featureType": "administrative",
+              "elementType": "geometry.stroke",
+              "stylers": [{
+                  "color": "#000000"
+              }, {
+                  "lightness": 17
+              }, {
+                  "weight": 1.2
+              }]
+          }]
+      };
 
-        // How you would like to style the map.
-        // This is where you would paste any style found on Snazzy Maps.
-        styles: [{
-            "featureType": "water",
-            "elementType": "geometry",
-            "stylers": [{
-                "color": "#000000"
-            }, {
-                "lightness": 17
-            }]
-        }, {
-            "featureType": "landscape",
-            "elementType": "geometry",
-            "stylers": [{
-                "color": "#000000"
-            }, {
-                "lightness": 20
-            }]
-        }, {
-            "featureType": "road.highway",
-            "elementType": "geometry.fill",
-            "stylers": [{
-                "color": "#000000"
-            }, {
-                "lightness": 17
-            }]
-        }, {
-            "featureType": "road.highway",
-            "elementType": "geometry.stroke",
-            "stylers": [{
-                "color": "#000000"
-            }, {
-                "lightness": 29
-            }, {
-                "weight": 0.2
-            }]
-        }, {
-            "featureType": "road.arterial",
-            "elementType": "geometry",
-            "stylers": [{
-                "color": "#000000"
-            }, {
-                "lightness": 18
-            }]
-        }, {
-            "featureType": "road.local",
-            "elementType": "geometry",
-            "stylers": [{
-                "color": "#000000"
-            }, {
-                "lightness": 16
-            }]
-        }, {
-            "featureType": "poi",
-            "elementType": "geometry",
-            "stylers": [{
-                "color": "#000000"
-            }, {
-                "lightness": 21
-            }]
-        }, {
-            "elementType": "labels.text.stroke",
-            "stylers": [{
-                "visibility": "on"
-            }, {
-                "color": "#000000"
-            }, {
-                "lightness": 16
-            }]
-        }, {
-            "elementType": "labels.text.fill",
-            "stylers": [{
-                "saturation": 36
-            }, {
-                "color": "#000000"
-            }, {
-                "lightness": 40
-            }]
-        }, {
-            "elementType": "labels.icon",
-            "stylers": [{
-                "visibility": "off"
-            }]
-        }, {
-            "featureType": "transit",
-            "elementType": "geometry",
-            "stylers": [{
-                "color": "#000000"
-            }, {
-                "lightness": 19
-            }]
-        }, {
-            "featureType": "administrative",
-            "elementType": "geometry.fill",
-            "stylers": [{
-                "color": "#000000"
-            }, {
-                "lightness": 20
-            }]
-        }, {
-            "featureType": "administrative",
-            "elementType": "geometry.stroke",
-            "stylers": [{
-                "color": "#000000"
-            }, {
-                "lightness": 17
-            }, {
-                "weight": 1.2
-            }]
-        }]
-    };
 
-    // Get the HTML DOM element that will contain your map
-    // We are using a div with id="map" seen below in the <body>
-    var mapElement = document.getElementById('map');
+      // Get the HTML DOM element that will contain your map
+      // We are using a div with id="map" seen below in the <body>
+      var mapElement = document.getElementById('map');
 
-    // Create the Google Map using out element and options defined above
-    map = new google.maps.Map(mapElement, mapOptions);
+      // Create the Google Map using out element and options defined above
+      map = new google.maps.Map(mapElement, mapOptions);
 
-    // Custom Map Marker Icon - Customize the map-marker.png file to customize your icon
-    var image = 'images/map-marker.png';
-    var myLatLng = new google.maps.LatLng(39.7376, -104.9897);
-    var beachMarker = new google.maps.Marker({
-        position: myLatLng,
-        map: map,
-        icon: image
-    });
+      // Custom Map Marker Icon - Customize the map-marker.png file to customize your icon
+      var image = 'images/map-marker.png';
+      var myLatLng = new google.maps.LatLng(location.lat, location.lng);
+      var beachMarker = new google.maps.Marker({
+          position: myLatLng,
+          map: map,
+          icon: image
+      });
 }
